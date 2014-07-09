384a5c880b0cb6cfd119ddeefc8254571dfd4825
Administrator <sherlock3@163.com>
2014-07-10 00:57:45 +0800

    hehe

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xwh
+ * Date: 14-7-8
+ * Time: 上午9:45
+ */
+define('MY_ROOT',dirname(__FILE__));
+require_once(MY_ROOT.'/init.php');
+$DB = database::getInstance();
+?>

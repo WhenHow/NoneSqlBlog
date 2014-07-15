8e5a45c2e226a5be1b737f9252b74a6e06cff6f1
Administrator <sherlock3@163.com>
2014-07-15 16:03:32 +0800

    hehe

--- admin/global.php
+++ admin/global.php
@@ -3,7 +3,6 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 14-7-15
- * Time: 下午1:12
+ * Time: 下午4:02
  */
-require_once("../init.php");
-require_once("./global.php");
+define('TEMPLATE_PATH', EMLOG_ROOT.'/admin/views/');
